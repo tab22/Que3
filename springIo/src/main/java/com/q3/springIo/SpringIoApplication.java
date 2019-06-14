e5c4db8c74afa27e3c49ddb0ae4546b8e5bc600e
Tabassum <tabs22sultana@gmail.com>
2019-06-14 16:38:46 +0530

    q3

---
+++ springIo/src/main/java/com/q3/springIo/SpringIoApplication.java
@@ -0,0 +1,13 @@
+package com.q3.springIo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIoApplication.class, args);
+	}
+
+}
